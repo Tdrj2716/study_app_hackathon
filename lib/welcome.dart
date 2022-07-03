1bbbc6f1d5feb2263cd9d70d2a8ad7c11ee5452a
HironoriUchibori <uchiborihironori963@gmail.com>
2022-07-03 13:17:22 +0900

    welcome画面のデザインを変更

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,58 +1,154 @@
 import 'package:flutter/material.dart';
 
-import 'login.dart';
-
 class Welcome extends StatelessWidget {
-  //const MyApp({Key? key}) : super(key: key);
+  const Welcome({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(250),
-        child: AppBar(
-            title: Image.asset(
-              'images/icon.png',
-              height: 60,
-              width: 60,
-            ),
-            backgroundColor: const Color(0xFFD1EEF3),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(48.0),
+      body: Column(
+        children: [
+          const Expanded(flex: 2, child: _TopPortion()),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    "選択してください",
+                    style: TextStyle(color: Color(0xFF696636)),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xFFD1EEF3),
+                          onPrimary: Color(0xFF696636),
+                        ),
+                        onPressed: () {},
+                        child: const Text('ルームを作る'),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: const Color(0xFFD1EEF3),
+                          onPrimary: const Color(0xFF696636),
+                        ),
+                        onPressed: () {},
+                        child: const Text('ルームに入る'),
+                      ),
+                    ],
+                  ),
+                  //const SizedBox(height: 16),
+                  //const _ProfileInfoRow()
+                ],
               ),
-            )),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-          child: Column(
+    );
+  }
+}
+
+class _ProfileInfoRow extends StatelessWidget {
+  const _ProfileInfoRow({Key? key}) : super(key: key);
+
+  final List<ProfileInfoItem> _items = const [
+    ProfileInfoItem("Posts", 900),
+    ProfileInfoItem("Followers", 120),
+    ProfileInfoItem("Following", 200),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      constraints: const BoxConstraints(maxWidth: 400),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: _items
+            .map((item) => Expanded(
+                    child: Row(
+                  children: [
+                    if (_items.indexOf(item) != 0) const VerticalDivider(),
+                    Expanded(child: _singleItem(context, item)),
+                  ],
+                )))
+            .toList(),
+      ),
+    );
+  }
+
+  Widget _singleItem(BuildContext context, ProfileInfoItem item) => Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            '選択してください',
-            style: TextStyle(color: Color(0xFF696636)),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              item.value.toString(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFFD1EEF3),
-                  onPrimary: Color(0xFF696636),
-                ),
-                onPressed: () {},
-                child: const Text('ルームを作る'),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: const Color(0xFFD1EEF3),
-                  onPrimary: const Color(0xFF696636),
-                ),
-                onPressed: () {},
-                child: const Text('ルームに入る'),
-              ),
-            ],
+          Text(
+            item.title,
+            style: Theme.of(context).textTheme.caption,
           )
         ],
-      )),
+      );
+}
+
+class ProfileInfoItem {
+  final String title;
+  final int value;
+  const ProfileInfoItem(this.title, this.value);
+}
+
+class _TopPortion extends StatelessWidget {
+  const _TopPortion({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(bottom: 50),
+          decoration: const BoxDecoration(
+              color: Color(0xFFD1EEF3),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
+              )),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            width: 120,
+            height: 120,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.fitWidth,
+                      image: AssetImage('images/icon.png'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
