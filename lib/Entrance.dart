ad353d5892581a601eaebc53400de5af3b657066
Yoshino <108174748+yoshinosub@users.noreply.github.com>
2022-07-03 15:34:44 +0900

    修正点を直しました。

--- lib/Entrance.dart
+++ lib/Entrance.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login.dart';　　
 
 class Entrance extends StatefulWidget {
   const Entrance({Key? key}) : super(key: key);
@@ -12,31 +13,62 @@ class _EntranceState extends State<Entrance> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('参加者入力'),
-        backgroundColor: Colors.blue,
-      ),
       body: SingleChildScrollView(
         reverse: true,
         child: Container(
-          padding: const EdgeInsets.all(1.0),
+          padding: const EdgeInsets.only(left:20,right:20,bottom:0,top:30),
+          color: Colors.white,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              _textField('ルームID:'),
-              _textField('パスワード:'),
-              _textField('名前:'),
-              _textField('目標:'),
+            children: [
               Container(
-                alignment: Alignment.bottomCenter,
-                child: ElevatedButton(
-                    child: Text('参加'),
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+                padding: const EdgeInsets.only(top:50,bottom:70),
+                decoration: BoxDecoration(
+                  color: Color(0xFFD1EEF3),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+
+               child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.topCenter,
+                      child: Text('入力してください',
+                        style: TextStyle(
+                          color: Color(0xFF693636),
+                          fontSize: 20,
+                        ),
                       ),
                     ),
-                    onPressed: () {}),
+                    _textField('ルームID:'),
+                    _textField('パスワード:'),
+                    _textField('名前:'),
+                    _textField('目標:'),
+
+
+
+                  ],
+               ),
+              ),
+              Container(
+                  alignment: Alignment.bottomCenter,
+                  padding: const EdgeInsets.only(top:30,bottom:0),
+                  child: ElevatedButton(
+                      child: Text('               参加する               ',
+                        style: TextStyle(
+                        color: Color(0xFF693636),
+                        fontSize: 20,
+                      ),
+                  ),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                          primary: Color(0xFFD1EEF3),
+
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, LogIn.id);
+                      })
               ),
             ],
           ),
@@ -47,13 +79,14 @@ class _EntranceState extends State<Entrance> {
 
   Widget _textField(String hintText) {
     return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
         child: TextField(
           decoration: InputDecoration(
-              fillColor: Colors.lightBlue[200],
+              fillColor: Colors.white,
               filled: true,
               border: OutlineInputBorder(),
               hintText: hintText),
         ));
+
   }
 }
